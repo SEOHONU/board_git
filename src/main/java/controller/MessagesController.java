723b494e9a4126490eb04464a707018d288302d7
SEOHONU <130737035+SEOHONU@users.noreply.github.com>
2023-04-17 16:56:43 +0900

    Update MessagesController.java

--- src/main/java/controller/MessagesController.java
+++ src/main/java/controller/MessagesController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.MessagesDAO;
 
-@WebServlet("*.message")
+@WebServlet("*.messages")
 public class MessagesController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
